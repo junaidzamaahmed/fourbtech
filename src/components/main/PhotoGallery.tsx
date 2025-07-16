80f136de7d10aed322a81d6fc3eec8fb7e73d4a5
hirokr <hrr3645@gmail.com>
2025-07-16 19:28:58 +0600

    refactor: streamline PhotoGallery component structure and improve grid class generation

--- src/components/main/PhotoGallery.tsx
+++ src/components/main/PhotoGallery.tsx
@@ -1,70 +1,68 @@
-import Image from "next/image"
+import Image from "next/image";
 
-import img1 from "../../../public/PhotoGallery/img1.png"
-import img2 from "../../../public/PhotoGallery/img2.png"
-import img3 from "../../../public/PhotoGallery/img3.png"
-import img4 from "../../../public/PhotoGallery/img4.png"
-import img5 from "../../../public/PhotoGallery/img5.png"
-import img6 from "../../../public/PhotoGallery/img6.png"
+import img1 from "../../../public/PhotoGallery/img1.png";
+import img2 from "../../../public/PhotoGallery/img2.png";
+import img3 from "../../../public/PhotoGallery/img3.png";
+import img4 from "../../../public/PhotoGallery/img4.png";
+import img5 from "../../../public/PhotoGallery/img5.png";
+import img6 from "../../../public/PhotoGallery/img6.png";
 
-const images = [img1, img2, img3, img4, img5, img6]
+const images = [img1, img2, img3, img4, img5, img6];
+const gridConfig = [
+  { colSpan: 3, rowSpan: 4, label: "Salmon" },
+  { colSpan: 3, rowSpan: 4, label: "Broccoli" },
+  { colSpan: 2, rowSpan: 4, label: "Tamago" },
+  { colSpan: 2, rowSpan: 3, label: "Pork" },
+  { colSpan: 4, rowSpan: 3, label: "Edamame" },
+  { colSpan: 2, rowSpan: 3, label: "Tomato" },
+];
+
+// Create a mapping for reliable class generation
+const getGridClasses = (colSpan, rowSpan) => {
+  const colSpanClasses = {
+    1: "col-span-1",
+    2: "col-span-2",
+    3: "col-span-3",
+    4: "col-span-4",
+    5: "col-span-5",
+    6: "col-span-6",
+    7: "col-span-7",
+    8: "col-span-8",
+  };
+
+  const rowSpanClasses = {
+    1: "row-span-1",
+    2: "row-span-2",
+    3: "row-span-3",
+    4: "row-span-4",
+    5: "row-span-5",
+    6: "row-span-6",
+    7: "row-span-7",
+  };
+
+  return `${colSpanClasses[colSpan]} ${rowSpanClasses[rowSpan]}`;
+};
 
 export default function PhotoGallery() {
   return (
-    <div className="min-h-screen bg-slate-800 py-16 px-4">
-      <div className="max-w-6xl mx-auto">
-        <div className="text-center mb-12">
-          <h1 className="text-5xl font-bold text-white mb-4">Gallery</h1>
-          <p className="text-slate-300 text-lg">We Meet, We Greet, We Enjoy, We Work</p>
-        </div>
-
-        <div className="grid gap-4">
-          {/* Mobile Layout */}
-          <div className="grid grid-cols-1 gap-4 sm:hidden">
-            {images.map((img, i) => (
-              <div key={i} className="aspect-[4/3]">
-                <Image
-                  src={img}
-                  alt={`Gallery ${i + 1}`}
-                  width={400}
-                  height={300}
-                  className="w-full h-full object-cover rounded-lg"
-                />
-              </div>
-            ))}
+    <div className="main-container">
+      <div className="grid grid-cols-8 grid-rows-7 gap-4 rounded-lg p-2 shadow-lg">
+        {images.map((src, i) => (
+          <div
+            key={i}
+            className={`relative ${getGridClasses(gridConfig[i].colSpan, gridConfig[i].rowSpan)} overflow-hidden rounded-lg shadow-md`}
+          >
+            <div className="h-5/6 min-h-56">
+              <Image
+                src={src}
+                alt={gridConfig[i].label}
+                fill
+                className="object-cover"
+              />
+            </div>
           </div>
-
-          {/* Tablet Layout */}
-          <div className="hidden sm:grid lg:hidden grid-cols-2 grid-rows-3 gap-4 h-[600px]">
-            <div className="row-span-1"><Image src={img1} alt="1" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="row-span-2"><Image src={img2} alt="2" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="row-span-1"><Image src={img3} alt="3" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="row-span-1"><Image src={img4} alt="4" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-2 row-span-1"><Image src={img5} alt="5" className="w-full h-full object-cover rounded-lg" /></div>
-          </div>
-
-          {/* Desktop Layout */}
-          <div className="hidden lg:grid xl:hidden grid-cols-12 grid-rows-4 gap-4 h-[600px]">
-            <div className="col-span-4 row-span-4"><Image src={img1} alt="1" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-4 row-span-4"><Image src={img2} alt="2" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-4 row-span-4"><Image src={img3} alt="3" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-3 row-span-4"><Image src={img4} alt="4" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-6 row-span-4"><Image src={img5} alt="5" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-3 row-span-4"><Image src={img6} alt="6" className="w-full h-full object-cover rounded-lg" /></div>
-          </div>
-
-          {/* XL Layout */}
-          <div className="hidden xl:grid grid-cols-16 grid-rows-6 gap-4 h-[600px]">
-            <div className="col-span-5 row-span-3"><Image src={img1} alt="1" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-6 row-span-2"><Image src={img2} alt="2" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-5 row-span-3"><Image src={img3} alt="3" className="w-full h-full object-cover rounded-lg" /></div>
-
-            <div className="col-span-4 row-span-3"><Image src={img4} alt="4" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-6 row-span-4"><Image src={img5} alt="5" className="w-full h-full object-cover rounded-lg" /></div>
-            <div className="col-span-6 row-span-3"><Image src={img6} alt="6" className="w-full h-full object-cover rounded-lg" /></div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
