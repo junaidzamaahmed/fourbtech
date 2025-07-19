053428272709f9b710454a5cffaa14ecaab3c310
junaidzamaahmed <junaidahmedzama@gmail.com>
2025-07-20 02:34:33 +0600

    refactor: update robots and sitemap files for consistent formatting and add dynamic export

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,36 +1,38 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
       lastModified: new Date(),
-      changeFrequency: "weekly"
+      changeFrequency: "weekly",
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
       lastModified: new Date(),
-      changeFrequency: "weekly"
+      changeFrequency: "weekly",
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
       lastModified: new Date(),
-      changeFrequency: "weekly"
+      changeFrequency: "weekly",
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/services/digital-marketing`,
       lastModified: new Date(),
-      changeFrequency: "weekly"
+      changeFrequency: "weekly",
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/services/mobile-apps`,
       lastModified: new Date(),
-      changeFrequency: "weekly"
+      changeFrequency: "weekly",
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/services/web-development`,
       lastModified: new Date(),
-      changeFrequency: "weekly"
+      changeFrequency: "weekly",
     },
   ];
 }
