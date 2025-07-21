b0e6a4ab12bbdbfa2aff34364b24560c54cc7871
hirokr <hrr3645@gmail.com>
2025-07-21 13:24:09 +0600

    feat: add EmployeeCard and FounderCard components with responsive design

---
+++ src/app/team/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FounderCard from "./_components/FounderCard";
+
+export default function page() {
+  return (
+    <div className="sm:mt-20 mainContainer">
+      
+      <FounderCard />
+    </div>
+  );
+}
