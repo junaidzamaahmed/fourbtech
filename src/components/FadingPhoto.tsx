8cf90c2af1e92e7f9e819fe4bc9f8c276db73df3
hirokr <hrr3645@gmail.com>
2025-07-23 19:10:31 +0600

    fix: adjust positioning of text overlay in FadingPhotoSlider component

--- src/components/FadingPhoto.tsx
+++ src/components/FadingPhoto.tsx
@@ -188,7 +188,7 @@ export const FadingPhotoSlider = ({
                     fill
                     className="object-cover"
                   />
-                  <div className="bg-opacity-50 absolute inset-0 flex items-center justify-center text-white bottom-10 left-20
+                  <div className="bg-opacity-50 absolute inset-0 flex items-center justify-center text-white ">
                     {alt}
                   </div>
                 </div>
