1c550e23a769e3956ecf98da7aadfe18fb09cdee
SarcasticSphinx <suhailahmedtohax@gmail.com>
2025-08-02 21:39:17 +0600

    fix: mapped the data 2 times for not getting out of data

--- src/components/main/ServiceBannersHome.tsx
+++ src/components/main/ServiceBannersHome.tsx
@@ -13,18 +13,20 @@ const Data: string[] = [
 
 export const ServiceBannersHome = ({
   direction,
+  borderColor,
 }: {
   direction: "left" | "right";
+  borderColor?: string;
 }) => {
   return (
     <GenericMarquee direction={direction} speed="normal" pauseOnHover={false}>
-      {Data.map((text, index) => (
-        <div
-          key={index}
-          className="flex items-center justify-center rounded-full border-[1px] border-white px-3 py-1 !text-sm"
-        >
-          {text}
-        </div>
+      {[...Data, ...Data].map((text, index) => (
+      <div
+        key={index}
+        className={`flex items-center justify-center rounded-full border-[1px] px-3 py-1 !text-sm border-${borderColor || "border-white"}`}
+      >
+        {text}
+      </div>
       ))}
     </GenericMarquee>
   );
